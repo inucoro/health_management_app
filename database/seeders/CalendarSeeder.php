e8babdf99366e4bf88ab3fb5bcd5f5d63fa6eaa5
inucoro <inucorotetu@gmail.com>
2024-05-22 21:13:02 +0900

    カレンダーの修正

--- database/seeders/CalendarSeeder.php
+++ database/seeders/CalendarSeeder.php
@@ -16,34 +16,21 @@ class CalendarSeeder extends Seeder
      */
     public function run()
     {
+        $today = (new DateTime())->format('Y-m-d');
+
         DB::table('calendars')->insert([
-            'id' => '4',
+            'id' => '1',
             'user_id' => '1',
-            'date' => '2024-04-15',
-            'record_body_weight' => '63',
-            'record_body_fat' => '13',
-            'ingestion_cal' => '1500',
-            'sum_movement_consumption_cal' => '300',
-            'record_calendar_memo' => '昨日は少し食べ過ぎた。',
+            'date' => $today,
+            'record_body_weight' => '0',
+            'record_body_fat' => '0',
+            'ingestion_cal' => '0',
+            'sum_movement_consumption_cal' => '0',
+            'record_calendar_memo' => '',
             'calendar_created_at' => new DateTime(),
             'calendar_updated_at' => new DateTime(),
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
-         ]);
-         
-        DB::table('calendars')->insert([
-            'id' => '5',
-            'user_id' => '1',
-            'date' => '2024-06-14',
-            'record_body_weight' => '64',
-            'record_body_fat' => '14',
-            'ingestion_cal' => '1000',
-            'sum_movement_consumption_cal' => '200',
-            'record_calendar_memo' => '昨日は少し食べ過ぎた。',
-            'calendar_created_at' => new DateTime(),
-            'calendar_updated_at' => new DateTime(),
-            'created_at' => new DateTime(),
-            'updated_at' => new DateTime(),
-         ]);
+        ]);
     }
 }
